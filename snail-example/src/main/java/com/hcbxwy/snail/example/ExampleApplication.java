27011624085dde52a6c7f96d3b9ec671b8be3fa1
Billson <Billson@hcbxwy.com>
2020-06-29 20:47:09 +0800

    新增演示项目

---
+++ snail-example/src/main/java/com/hcbxwy/snail/example/ExampleApplication.java
@@ -0,0 +1,19 @@
+package com.hcbxwy.snail.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 演示项目
+ *
+ * @author Billson
+ * @date 2020/6/29 20:45
+ */
+@SpringBootApplication
+public class ExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleApplication.class, args);
+    }
+
+}
---
+++ snail-example/src/test/java/com/hcbxwy/snail/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcbxwy.snail.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
